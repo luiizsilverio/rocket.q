ca0cddf11034e9cac81ed9851506c20e8d3adaba
luiizsilverio <luiiz.silverio@gmail.com>
2021-07-09 11:40:35 -0300

    Workshop 4

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,16 +3,35 @@ import Modal from './modal.js'
 const checkButtons  = document.querySelectorAll('.actions .check')
 const deleteButtons = document.querySelectorAll('.actions .delete')
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+function handleClick(event, deletar = false) {
+  event.preventDefault()
+  
+  if (deletar) {
+    modalTitle.innerHTML = 'Excluir Pergunta'
+    modalDescription.innerHTML = 'Tem certeza que deseja excluir esta pergunta?'
+    modalButton.innerHTML = 'Sim, excluir'
+    modalButton.classList.add("red")
+  } else {
+    modalTitle.innerHTML = 'Marcar como Lida'
+    modalDescription.innerHTML = 'Tem certeza que deseja marcar como lida?'
+    modalButton.innerHTML = 'Sim, marcar'
+    modalButton.classList.remove("red")
+  }
+  Modal.open()
+}
+
 checkButtons.forEach(button => (
-  button.addEventListener('click', event => {
-    Modal.open()
-  })
+  button.addEventListener('click', event => 
+    handleClick(event)    
+  )
 ))
 
 deleteButtons.forEach(button => (
-  button.addEventListener('click', event => {
-    Modal.open()
-  })
+  button.addEventListener('click', event => 
+    handleClick(event, true)    
+  )
 ))
-
-1:29
--- src/route.js
+++ src/route.js
@@ -5,5 +5,10 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-room', (req, res) => res.render("create-room"))
+route.get('/room/:room/:question/:action', (req, res) => {
+  res.send(req.params)
+})
 
 module.exports = route
+
+1:55
