371372d4393e5a01e9410371d67b82f27234f82a
luiizsilverio <luiiz.silverio@gmail.com>
2021-07-11 21:34:04 -0300

    Workshop 5

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,10 +1,27 @@
 const Database = require('../db/config')
 
 module.exports = {   
-   index(req, res) {
+   async index(req, res) {
+      const db = await Database()
       const { room, question, action } = req.params
       const password = req.body.password
-  
+        
+      const theRoom = await db.get(`SELECT * FROM rooms WHERE id = ${room}`)
+
+      if (theRoom && theRoom.password === password) {
+         if (action === "delete") {
+            await db.run(`DELETE FROM questions WHERE id = ${question}`)
+         } 
+         else if (action === "check") {
+            await db.run(`
+               UPDATE questions SET read = 1 
+               WHERE id = ${question}
+            `)
+         }
+      }
+
+      await db.close()
+      res.redirect(`/room/${room}`)
    },
    
    async create(req, res) {
